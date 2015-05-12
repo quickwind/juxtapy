294a0d1a3a00c32a809fe9140e4916f991cdd8ef
Timothy Davenport <tmthydvnprt@users.noreply.github.com>
2015-05-11 19:00:13 -0700

    updated test script

--- test.sh
+++ test.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/bash
 
-echo 'Tests' > test_report.txt
-echo ' ' >> test_report.txt
+# test
+nosetests tests -v -d --with-coverage --cover-package=juxtapy,tests --cover-tests --cover-erase --cover-inclusive --cover-branches &> test.txt.temp
 
-nosetests tests -v -d --with-coverage --cover-package=juxtapy,tests --cover-tests --cover-erase --cover-inclusive --cover-branches &> test_report.txt
+# test report
+echo '' > test_report.txt
+echo 'Juxtapy Testing Report' >> test_report.txt
+echo `date "+%Y-%m-%d %H:%M:%S %z"` >> test_report.txt
+echo '=========================================' >> test_report.txt
+echo '' >> test_report.txt
+echo 'Test Report' >> test_report.txt
+cat test.txt.temp >> test_report.txt
 
 rm .coverage
+rm test.txt.temp
+
+echo 'project tested'
